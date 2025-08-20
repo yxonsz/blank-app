36a5b20ad8c6375d44582a57978172d943b9e08d
이연수 <yxxn_su@naver.com>
2025-08-20 03:00:22 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title(" 🏫1-2 시간표 공지 ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "1학년 2반의 시간표를 확인해보세요 -> [미림마이스터고 시간표](https://docs.google.com/spreadsheets/d/1Ow7PfJ_txFYUcS216SmtGtpYcpSK-ZytNWdP-VCiwWw/edit?pli=1&gid=556197737#gid=556197737)."
 )
